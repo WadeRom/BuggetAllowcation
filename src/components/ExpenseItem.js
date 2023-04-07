0ec13da5c22d4bcc261450a191c8acebd3d903fa
waly <wclrosromero@gmail.com>
2023-04-07 13:47:53 -0600

    completed

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,1 +1,42 @@
 
+import { TiDelete } from 'react-icons/ti';
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+
+const ExpenseItem = (props) => {
+  const { dispatch } =  useContext(AppContext);
+  
+  const handleDelete = () => {
+    
+    dispatch({
+      type: "DELETE_EXPENSE",
+      payload: props.id
+    })
+
+  }
+  
+  const increaseAllocation = (name) => {
+    
+    const expense = {
+      name: name,
+      cost: 10
+    }
+    
+    dispatch({
+      type: "ADD_EXPENSE",
+      payload: expense
+    })
+
+  }
+
+  return(
+    <tr>
+      <td>{props.name}</td>
+      <td>{props.cost}</td>
+      <td><button onClick={event => increaseAllocation(props.name)} >+</button></td>
+      <td><button><TiDelete size='1.5rem' onClick={handleDelete}></TiDelete></button></td>
+    </tr>
+  )
+}
+
+export default ExpenseItem;
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,1 +1,36 @@
+import React, {useContext} from "react";
+import ExpenseItem from "./ExpenseItem";
+import { AppContext } from "../context/AppContext";
 
+const ExpenseList = () => {
+  const { expenses } =  useContext(AppContext);
+
+  return(
+    
+    <table className="table">
+      
+      <thead className="thead-light">
+        <tr className="col">Departament</tr>
+        <tr className="col">Allocated Budget</tr>
+        <tr className="col">Increase</tr>
+        <tr className="col">Delete</tr>
+      </thead>
+
+      <tbody>
+        {
+          expenses.map((expense) => (
+            <ExpenseItem id={expense.id} 
+              key={expense.id} 
+              name={expense.name} 
+              cost={expense.cost} 
+            />
+          ))
+        }
+      </tbody>
+      
+    </table>
+
+  )
+}
+
+export default ExpenseList;
